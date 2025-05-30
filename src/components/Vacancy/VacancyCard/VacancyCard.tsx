aa4831a62b749ec60f082cb1775e2a63449e2003
Ar4ySmoker <your_email@example.com>
2025-05-30 11:59:15 +0300

    пробуем обойти ограничения на оптимизайию изображений разместив их на фоне

--- src/components/Vacancy/VacancyCard/VacancyCard.tsx
+++ src/components/Vacancy/VacancyCard/VacancyCard.tsx
@@ -94,7 +94,6 @@
 // }
 'use client';
 
-import Image from "next/image";
 import { Button } from "@/components/ui/button";
 import {
   Card,
@@ -104,13 +103,11 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
-import { AspectRatio } from "@radix-ui/react-aspect-ratio";
 import { Car, FileStack, HandCoins, Home, MapPinned } from "lucide-react";
 import { Drawer, DrawerTrigger } from "@/components/ui/drawer";
 import DrawerContentComponent from "@/components/Drawer/DrawerContentComponent";
 import { Dialog } from "@/components/ui/dialog";
 import { VacancyType } from "@/lib/types/vacancy"; // Тип вакансии
-import { Ripple } from "@/components/magicui/ripple";
 
 export default function VacancyCard({ vacancy }: { vacancy: VacancyType }) {
   return (
@@ -118,30 +115,19 @@ export default function VacancyCard({ vacancy }: { vacancy: VacancyType }) {
     <Dialog>
       <Drawer>
         <Card className="w-full max-w-[400px] h-full relative pt-0!">
-         <div className="relative flex py-10  w-full flex-col items-center justify-center overflow-hidden rounded-lg bg-background">
-      <p className="z-10 whitespace-pre-wrap text-center text-3xl font-medium tracking-tighter text-primary">
-        {vacancy?.title}
+     <div className="relative flex  w-full flex-col items-center justify-center overflow-hidden rounded-lg drop-shadow-md  bg-center bg-no-repeat bg-cover"
+     style={{
+    backgroundImage: `url(${vacancy.imageFB || '/default.jpg'})`,
+  }}
+     >
+      <p className="z-10 p-10 my-auto w-full h-full min-h-[200px] whitespace-pre-wrap text-center text-3xl font-medium tracking-tighter ">
+        {/* {vacancy?.title} */}
       </p>
-      <Ripple mainCircleSize={50} mainCircleOpacity={0.5} />
     </div>
-        {/* <Image
-          src="/main/logofordafaultimage.jpg"
-          alt="icon"
-          width={400}
-          height={400}
-          className="h-50 w-100 rounded-lg bg-muted shrink-0"
-        /> */}
-          {/* <AspectRatio ratio={16 / 9}>
-            <Image
-              src={vacancy?.imageFB || "/main/logofordafaultimage.jpg"}
-              alt="Vacancy"
-              width={400}
-              height={200}
-              className="rounded-md object-cover"
-            />
-          </AspectRatio> */}
+         
           <CardHeader className="mt-6">
-            {/* <CardTitle className="text-xl">{vacancy?.title}</CardTitle> */}
+            
+            <CardTitle className="text-xl">{vacancy?.title}</CardTitle>
             <CardDescription>{vacancy?.roof_type}</CardDescription>
           </CardHeader>
 
