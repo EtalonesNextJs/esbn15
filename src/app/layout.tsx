8752a833498172c13042d7945b17d9a2da34a563
EtalonesNextJs <etalonesnextjs@gmail.com>
2025-06-11 09:58:15 +0300

    addVercel analitic

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import DefaultLayout from "@/layouts/Default";
-
+import { Analytics } from '@vercel/analytics/next';
 
 
 const geistSans = Geist({
@@ -32,6 +32,7 @@ export default function RootLayout({
         >
       <DefaultLayout>
         {children}
+        <Analytics />
       </DefaultLayout>
       </body>
     </html>
