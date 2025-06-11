b95085ac0258169be8e54ea45ea90809110ecd9c
EtalonesNextJs <etalonesnextjs@gmail.com>
2025-06-11 10:05:14 +0300

    add SpeedInsights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import DefaultLayout from "@/layouts/Default";
 import { Analytics } from '@vercel/analytics/next';
-
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -33,6 +33,7 @@ export default function RootLayout({
       <DefaultLayout>
         {children}
         <Analytics />
+        <SpeedInsights />
       </DefaultLayout>
       </body>
     </html>
